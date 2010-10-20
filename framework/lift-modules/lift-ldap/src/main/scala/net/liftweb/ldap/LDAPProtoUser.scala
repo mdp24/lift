0f5c3fd6264c2da2a7d8534034e63bfa9c4dcca2
David Pollak <feeder.of.the.bears@gmail.com>
2010-10-19 21:41:22 -0700

    Minor fix to 684 for LDAP

--- framework/lift-modules/lift-ldap/src/main/scala/net/liftweb/ldap/LDAPProtoUser.scala
+++ framework/lift-modules/lift-ldap/src/main/scala/net/liftweb/ldap/LDAPProtoUser.scala
@@ -35,10 +35,10 @@ import Helpers._
 trait MetaLDAPProtoUser[ModelType <: LDAPProtoUser[ModelType]] extends MetaMegaProtoUser[ModelType] {
     self: ModelType =>
 
-    override def signupFields: List[BaseOwnedMappedField[ModelType]] = uid ::
+    override def signupFields: List[FieldPointerType] = uid ::
         cn :: dn :: Nil
 
-    override def fieldOrder: List[BaseOwnedMappedField[ModelType]] = uid ::
+    override def fieldOrder: List[FieldPointerType] = uid ::
         cn :: dn :: Nil
 
     /**
